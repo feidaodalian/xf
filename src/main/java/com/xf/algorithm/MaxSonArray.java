42b8192ce29cfce64c63f2065115ea5df3eea48a
feixu <feixu@sohu-inc.com>
2016-02-24 17:35:42 +0800

    设计模式

--- src/main/java/com/xf/algorithm/MaxSonArray.java
+++ src/main/java/com/xf/algorithm/MaxSonArray.java
@@ -20,8 +20,20 @@ public class MaxSonArray {
         }
         System.out.println(max);
     }
+    public static void handle2(int a[]){
+        int max=a[0],temp=a[0],length=a.length;
+        for(int i=1;i<length;i++){
+                temp+=a[i];
+                if(max<temp&&max>0){
+                    max = temp;
+                }else if(max<0&&temp<0){
+                    temp = 0;
+                }
+            }
+        System.out.println(max);
+    }
     public static void main(String[] args) {
         int a[]={33,2,-100,3,111,-90,0,22,2,99,-9,100,33,1111,333,0,-99,-1000,88888};
-         handle(a);
+         handle2(a);
     }
 }
